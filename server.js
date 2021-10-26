2a7eefcae2a74d2a73e35033afb216b719cf6a62
Aishaml1 <Aisha.lugg1@icloud.com>
2021-10-25 22:36:10 -0400

    add data to flowers

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-import express from "express";
+import Express from "express";
 
-const app = express()
+const app = Express();
+
+// HTTP --> GET, POST, PUT, DELETE
+// GET -> how you get informations -> app.get()
+// Put -> how u change information -> app.put
+// Post -> how you change or add information app.post()
+// Delete -> how you delete information app.delete
 
 app.set('view engine', 'ejs')
 
-app.get('/flowers', function(req, res) {
+//request & response
+// request = all information coming in
+// send information = respond
 
-    res.send('<h1> testing </h1>')
+app.get('/flowers', function(req, res) {
+res.render('flowers')
 })
 
 app.listen(3000, function(){
